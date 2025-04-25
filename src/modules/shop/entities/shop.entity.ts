cabe5fd856086cb97e49a6c544387767fec9db75
yongsin-oak <64070217@it.kmitl.ac.th>
2025-04-25 19:22:05 +0700

    last

---
+++ src/modules/shop/entities/shop.entity.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Shop {
+  @ApiProperty()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column({ unique: true })
+  name: string;
+
+  @ApiProperty()
+  @Column({ nullable: true })
+  description: string;
+
+  @OneToMany(() => Product, (product) => product.shop, {
+    nullable: true,
+  })
+  products: Product[];
+
+  @ApiProperty()
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn({
+    type: 'timestamp',
+  })
+  updatedAt: Date;
+}
